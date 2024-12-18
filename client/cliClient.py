53a5069bc6e84b35e4f430feca4e8348552583a9
NeoCortex97 <stefan_schmelz@gmx.net>
2024-12-19 00:09:59 +0100

    implemented primitive cli client to issue single requests.

---
+++ client/cliClient.py
@@ -0,0 +1,23 @@
+import zmq
+
+def main():
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)
+    print('connection to socket ...', end='')
+    socket.connect('tcp://127.0.0.1:6660')
+    print('OK')
+    while True:
+        name = input('name: ')
+        space = input('space: ')
+        logo = input('logo: ')
+        url = input('website: ')
+
+        print('processing request ...', end='')
+        socket.send(f'TAG;{name};{space};{logo};{url}'.encode('utf-8'))
+        print(socket.recv().decode('utf-8'))
+        print('#' * 80)
+
+
+
+if __name__ == '__main__':
+    main()
