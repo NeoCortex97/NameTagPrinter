8c43767851df47c08f5c4e092942116471acd3dd
NeoCortex97 <stefan_schmelz@gmx.net>
2024-12-21 12:53:01 +0100

    fixed asset paths in receipt driver

--- server/printer/receipotDriver.py
+++ server/printer/receipotDriver.py
@@ -1,7 +1,12 @@
+import pathlib
 from tempfile import mktemp
 
 import qrcode
 from escpos.printer import Usb
+
+
+PROJECT_ROOT = pathlib.Path(__file__).parent.parent.parent
+ASSET_ROOT = PROJECT_ROOT.joinpath('assets')
 
 
 class ReceiptDriver:
@@ -9,7 +14,7 @@ class ReceiptDriver:
         self.printer = Usb(0x04b8, 0x0202, 0, profile="TM-T88III")
 
     def print(self, name, space, logo, url):
-        self.printer.image('../assets/images/receipt/flipdot.png', center=True)
+        self.printer.image(ASSET_ROOT.joinpath('images/receipt/flipdot.png'), center=True)
         self.printer.text('\n\n\n')
 
         self.printer.textln(f'Space:\t{space}\n')
